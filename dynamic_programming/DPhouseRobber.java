165f6558efaee773e38e406498a45935e2d15043
yavar29 <yavarkhan1997@gmail.com>
2020-11-21 10:19:34 +0530

    modified program DPhouseRobber.java

--- dynamic_programming/DPhouseRobber.java
+++ dynamic_programming/DPhouseRobber.java
@@ -1,30 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int houseRobber(int houses[]){
-        if(houses.length==0){
-            return 0;
-        }
-        if (houses.length == 1) {
-            return houses[0];
-        }
-        if(houses.length==2){
-            return Math.max(houses[0], houses[1]);
-        }
-
-        int[] DP=new int[houses.length];
-
-        for(int i=2; i<houses.length; i++){
-            DP[0]=0;
-            DP[1]=houses[1];
-            DP[i]=Math.max(DP[i-2] +houses[i],DP[i-1]);
-        }
-        return DP[houses.length-1];
+    public static int houseRobber(int nums[]){
+        if(nums.length ==0) return 0;
+    	//if(nums.length == 1) return nums[0];
+    
+    	int[] dp = new int[nums.length];
+    	dp[0] = nums[0]; 
+    	dp[1] = Math.max(nums[0], nums[1]); 
+    
+    	for(int i = 2; i < nums.length; i++){
+        	dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);   
+    	}
+    
+    	return dp[dp.length - 1];
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int houses[]={1,5,2,1,11,9};
-        System.out.println(houseRobber(houses));
+        int nums[]={1,5,2,1,11,9};
+        System.out.println(houseRobber(nums));
     }
 }
